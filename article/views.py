5d56ac95f76ce6fe785dd56d6462c3068e74acae
pengcongfa <1583071538@qq.com>
2018-01-25 10:40:18 +0800

    每篇博客一个页面

--- article/views.py
+++ article/views.py
@@ -1,18 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
-
+from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
-def detail(request, my_args):
-    Article.objects.create(title='hello,world', category='python', content='let us add database item')
-    Article.objects.create(title='django blog study', category='python', content='django blog tutorial')
-    post = Article.objects.all()[int(my_args)]
-    str = ("title= %s,category = %s,date_time = %s,content = %s"
-           % (post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str)
+# def detail(request, my_args):
+#     Article.objects.create(title='hello,world', category='python', content='let us add database item')
+#     Article.objects.create(title='django blog study', category='python', content='django blog tutorial')
+#     post = Article.objects.all()[int(my_args)]
+#     str = ("title= %s,category = %s,date_time = %s,content = %s"
+#            % (post.title, post.category, post.date_time, post.content))
+#     return HttpResponse(str)
 
 
 def home(request):
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
+
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post})
